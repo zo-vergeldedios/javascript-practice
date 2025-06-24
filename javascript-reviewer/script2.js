778b3bfbe95dfe1689b8feba3998e920990fc8f5
Zo <zo.vergeldedios@gmail.com>
2025-06-24 10:31:18 -0700

    Practicing array methods

--- javascript-reviewer/script2.js
+++ javascript-reviewer/script2.js
@@ -69,3 +69,157 @@ const cartCalc = cart2.reduce((num, cost) => {
 }, 0);
 
 console.log(`$${cartCalc.toFixed(2)}`);
+
+const numbers = nums.map((num) => num * 2);
+console.log(numbers);
+
+const words = ["the", "banana", "at", "window"];
+
+const filteredWords = words.filter((long) => {
+  return long.length >= 4;
+});
+
+console.log(filteredWords);
+
+const nbrs = [1, 4, 6, 7, 10, 13];
+
+const evenNumbers = nbrs.reduce((acc, numbers) => {
+  if (numbers % 2 === 0) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(evenNumbers);
+
+const wordss = ["strength", "gym", "banana", "code", "deadlift", "HIIT"];
+
+const fiveLetters = wordss.reduce((acc, word) => {
+  if (word.length >= 5) {
+    acc++;
+  }
+  return acc;
+}, 0);
+
+console.log(fiveLetters);
+
+const people2 = [
+  { name: "Zo", age: 29 },
+  { name: "Ryu", age: 34 },
+  { name: "Lex", age: 22 },
+  { name: "Aria", age: 30 },
+  { name: "Max", age: 29 },
+  { name: "Kai", age: 22 },
+];
+// Expected output
+// {
+//   22: 2,
+//   29: 2,
+//   30: 1,
+//   34: 1
+// }
+
+const output = people2.reduce(
+  (acc, person) => {
+    if (person.age == 22) {
+      acc[22] += 1;
+    } else if (person.age == 29) {
+      acc[29] += 1;
+    } else if (person.age == 30) {
+      acc[30] += 1;
+    } else if (person.age == 34) {
+      acc[34] += 1;
+    }
+    return acc;
+  },
+  {
+    22: 0,
+    29: 0,
+    30: 0,
+    34: 0,
+  }
+);
+
+// console.log(output);
+
+// const output2 = people.reduce((acc, person) => {
+//   acc[person.age] = (acc[person.age] || 0) + 1;
+//   return acc;
+// }, {});
+
+const people = [
+  { name: "Zo", age: 29 },
+  { name: "Ryu", age: 34 },
+  { name: "Lex", age: 22 },
+  { name: "Aria", age: 30 },
+];
+
+const result = people.reduce(
+  (acc, person) => {
+    acc.totalAge += person.age;
+    if (person.age > acc.oldest.age) {
+      acc.oldest = person;
+    }
+    return acc;
+  },
+  { totalAge: 0, oldest: { name: "", age: 0 } }
+);
+
+console.log(result);
+// { totalAge: 115, oldest: { name: "Ryu", age: 34 } }
+
+const people3 = [
+  { name: "Zo", age: 29 },
+  { name: "Ryu", age: 34 },
+  { name: "Lex", age: 22 },
+  { name: "Aria", age: 30 },
+  { name: "Max", age: 29 },
+  { name: "Kai", age: 22 },
+];
+
+// const output3 = people3.reduce((acc, person) => {
+//   if (!acc[person.age]) {
+//     acc[person.age] = [];
+//   }
+//   acc[person.age].push(person.name);
+
+//   return acc;
+// }, {});
+
+// console.log(output3);
+
+const output4 = people3.reduce((acc, person) => {
+  if (!acc[person.age]) {
+    acc[person.age] = [];
+  }
+  acc[person.age].push(person);
+  return acc;
+}, {});
+
+console.log(output4);
+
+const users = [
+  { name: "Zo", score: 88, active: true },
+  { name: "Lex", score: 52, active: false },
+  { name: "Ryu", score: 91, active: true },
+  { name: "Aria", score: 77, active: true },
+  { name: "Kai", score: 69, active: false },
+  { name: "Max", score: 95, active: true },
+];
+
+const output5 = users
+  .filter((user) => {
+    return user.active == true;
+  })
+  .sort((a, b) => {
+    return b.score - a.score;
+  })
+  .map((person) => {
+    return {
+      name: person.name,
+      score: person.score,
+    };
+  });
+
+//
+console.log(output5);
